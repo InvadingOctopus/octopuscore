d0f62907daaf9960b66d4c15b87e2e0b487a14ae
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2023-08-04 00:25:27 +0400

    Update package manifest
    Rename OctopusKit to OctopusCore

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-//  https://github.com/InvadingOctopus/octopuskit
+//  https://github.com/InvadingOctopus/octopuskit-core
 
 import PackageDescription
 
 let package = Package(
-    name: "OctopusKit",
+    name: "OctopusCore",
     defaultLocalization: "en",
     platforms: [
         .iOS(.v17),
@@ -16,8 +16,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "OctopusKit",
-            targets: ["OctopusKit"]),
+            name: "OctopusCore",
+            targets: ["OctopusCore"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -27,12 +27,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "OctopusKit",
+            name: "OctopusCore",
             dependencies: [],
             exclude: [
                 "Apple API Extensions/SwiftUI/OctopusUI.md"],
             resources: [
-                .copy("Assets/Shaders/ShaderKit/LICENSE")]
+                ]
 //          , swiftSettings: [                // MARK: - Conditional Compilation Flags
 //                .define("LOGECSVERBOSE"),   // Log detailed ECS core events. ⚠️ May decrease performance.
 //                .define("LOGECSDEBUG"),     // Log ECS debugging info. ⚠️ Will decrease performance.
@@ -43,8 +43,8 @@ let package = Package(
 //            ]                               // Remember to uncomment this if you uncomment any of the lines above ^^
         ),
         .testTarget(
-            name: "OctopusKitTests",
-            dependencies: ["OctopusKit"]),
+            name: "OctopusCoreTests",
+            dependencies: ["OctopusCore"]),
     ],
     swiftLanguageVersions: [.v5]
 )
