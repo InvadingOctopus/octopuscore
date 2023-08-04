8422954b88174bc04fa9882011ecdf807a5c23e6
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2023-08-04 17:41:18 +0400

    Rename OctopusKit-Core to OctopusCore in manifest

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-// https://github.com/InvadingOctopus/octopuskit-core
+// https://github.com/InvadingOctopus/octopuscore
 
 import PackageDescription
 
