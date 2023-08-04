48504f57981d2699c93e8fd04917a3958fc45851
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2023-08-04 04:04:11 +0400

    Remove OctopusUI stubs

--- Sources/OctopusCore/SwiftUI/OctopusUI.swift
+++
@@ -1,9 +0,0 @@
-//
-//  OctopusUI.swift
-//
-//  Created by ShinryakuTako@invadingoctopus.io on 2019/11/20
-//
-
-//  For a library of open-source SwiftUI controls and extensions, see the OctopusUI repository:
-//
-//  https://github.com/InvadingOctopus/octopusui
